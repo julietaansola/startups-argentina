60bfd940b27881f25c354367fe7e6e071f171674
julieta <julietaansolaberraute@gmail.com>
2020-07-16 17:18:12 -0300

    add router-dom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Prueba from "./pages/prueba";
+import AgregarStartup from "./pages/agregarstartup";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Router>
+		<Route exact path="/" component={App} />
+		<Route exact path="/prueba" component={Prueba} />
+		<Route exact path="/agregarstartup" component={AgregarStartup} />
+	</Router>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//Router > Su unica funcion es englobar a TODAS las rutas. Inicio y fin de rutas en una aplicacion. ROUTE es para especificar las rutas
+
+/*
+    <Route path="/" component={App} />  path="/" < Ruta inexacta
+Todos los componentes que utilizamos como pages, van en pages.
+Todos los componentes que utilizamos a lo largo del prjecto van en components
+ */
